4462d61a727eff790f228885451f9643136f4111
Toad44Sage <124910234+Toad44Sage@users.noreply.github.com>
2023-10-05 09:50:38 +0400

    ''

--- Tern1.js
+++ Tern1.js
@@ -1,21 +1,32 @@
 let a = Math.floor(Math.random() * 100);
-console.log(`a = ${a}`);
-
+let result;
 if (a > 10) {
-  if (a > 5) {
-    console.log(2 * a + 1);
-  } else {
-    console.log(5);
-  }
-} else if (a < 3) {
-  console.log(1);
-} else if (a > 6) {
-  console.log(5);
-} else if (a % 2 == 0) {
-  console.log(6);
+  result = a;
 } else {
-  console.log(7);
+  result = a * 2;
 }
+
+if (result > 5) {
+  result = 2 * a + 1;
+} else {
+  if (a < 3) {
+    result = 1;
+  } else {
+    result = 2 * (a - 2);
+  }
+
+  if (result > 4) {
+    result = 5;
+  } else {
+    if (a % 2 === 0) {
+      result = 6;
+    } else {
+      result = 7;
+    }
+  }
+}
+console.log(a);
+console.log(result);
 
 // условие с условным (тернарным) оператором перевести в if...else И switch()
 // результат выводить в консоль, с пощью console.log()
